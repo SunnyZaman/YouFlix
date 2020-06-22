c7f3b0dbd2423cd3014aca4961c4e8c9c9f0fd9e
Zaman <sjzaman@ryerson.ca>
2020-06-21 20:20:31 -0400

    gaming videos

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -14,7 +14,7 @@ import Carousel from "../components/Carousel";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
-import { concat, of } from "rxjs";
+import { forkJoin, of } from "rxjs";
 import { ajax } from "rxjs/ajax";
 import { map, catchError } from "rxjs/operators";
 
@@ -33,51 +33,8 @@ export default {
     };
   },
   methods: {
-    //     async function getUserAsync(name)
-    // {
-    //   let response = await fetch(`https://api.github.com/users/${name}`);
-    //   let data = await response.json()
-    //   return data;
-    // }
-
-    // getUserAsync('yourUsernameHere')
-    //   .then(data => console.log(data));
     init() {
-      // this.getTrendingVideos().then(data => {
-      //   this.featuredVideo = data.items[0];
-      //   const vids = data.items;
-      //   console.log(vids);
-
-      //   while (vids.length > 0) {
-      //     this.trending.push(vids.splice(0, 5));
-      //   }
-      //   console.log(this.trending);
-
-      //   console.log(data);
-      // });
       this.getVideoCategories();
-
-      //  this.fetchPigeons().then(() => console.log(this.featuredVideo));
-
-      // fetch(
-      //   `${process.env.VUE_APP_VIDEO_ENDPOINT}?key=${process.env.VUE_APP_API_KEY}&part=snippet&chart=mostPopular&maxResults=35`
-      // )
-      //   .then(res => {
-      //     return res.json();
-      //   })
-      //   .then(this.setResults);
-
-      // Promise.all([
-      //   fetch(url1).then(value => value.json()),
-      //   fetch(url2).then(value => value.json())
-      //   ])
-      //   .then((value) => {
-      //      console.log(value)
-      //     //json response
-      //   })
-      //   .catch((err) => {
-      //       console.log(err);
-      //   });
     },
     getVideoCategories() {
       this.dataLoaded = false;
@@ -101,45 +58,19 @@ export default {
         })
       );
       const obsArray$ = [trending$, gaming$];
-      concat(...obsArray$).subscribe({
+      forkJoin(...obsArray$).subscribe({
         complete: () => {
           this.dataLoaded = true;
         }
       });
     },
-    //     async fetchPigeons(){
-    //     const fetchCall = await fetch(`${process.env.VUE_APP_VIDEO_ENDPOINT}?key=${process.env.VUE_APP_API_KEY}&part=snippet&chart=mostPopular&maxResults=35`)
-    //     .then(res => res.json())
-    //     .then(res => {
-    //         console.log(res.items); // Here I get what I need
-    //         this.featuredVideo = res.items[0];
-    //     })
-    //     return fetchCall;
-    // },
     getVideos(endpoint, filter) {
       const url = `${endpoint}?key=${process.env.VUE_APP_API_KEY}&${filter}`;
       return ajax.getJSON(url).pipe(
         map(response => response),
         catchError(error => of(error))
       );
-      // let response = await fetch(
-      //   `${process.env.VUE_APP_VIDEO_ENDPOINT}?key=${process.env.VUE_APP_API_KEY}&part=snippet&chart=mostPopular&maxResults=35`
-      // );
-
-      // return response.json();
     }
-    // setResults(results) {
-    //   console.log(JSON.parse(JSON.stringify(results.items)));
-    //   this.featuredVideo = results.items[0];
-    //   console.log(JSON.parse(JSON.stringify(results.items)));
-    //   const vids = JSON.parse(JSON.stringify(results.items));
-    //   while (vids.length > 0) {
-    //     this.trending.push(vids.splice(0, 5));
-    //   }
-    //   console.log(this.trending);
-    //   this.trending = JSON.parse(JSON.stringify(this.trending));
-    //   console.log(this.trending);
-    // }
   },
 
   mounted() {
