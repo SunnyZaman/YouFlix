ff40928b64a2376d282328cc5ba13ed7d820c45e
Zaman <sjzaman@ryerson.ca>
2020-06-29 18:01:40 -0400

    loading on search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,32 +1,37 @@
 <template>
   <div class="search-container">
-    <v-row v-if="videos.length>0">
-      <v-col
-        class="pa-1"
-        v-for="(video,j) in videos"
-        :key="'videso'+j"
-        xl="2"
-        lg="2"
-        md="3"
-        sm="4"
-        cols="6"
-      >
-        <router-link :to="{ name: 'watch', params: { v: video.id.videoId }}">
-          <v-card class="video-card" height="150">
-            <v-img :src="video.snippet.thumbnails.high.url" width="100%" height="100%"></v-img>
-            <div class="video-overlay">
-              <p>{{video.snippet.title}}</p>
-            </div>
-          </v-card>
-        </router-link>
-      </v-col>
-    </v-row>
-    <div v-else class="no-results-container">
-      <p>Your search for "{{$route.params.q}}" did not have any matches.</p>
-      <p>Suggestions:</p>
-      <ul >
-        <li v-for="suggestion in suggestions" :key="suggestion">{{suggestion}}</li>
-      </ul>
+    <div v-if="loading">
+      <v-progress-circular :size="70" :width="7" color="red" indeterminate></v-progress-circular>
+    </div>
+    <div v-else>
+      <v-row v-if="videos.length>0">
+        <v-col
+          class="pa-1"
+          v-for="(video,j) in videos"
+          :key="'videso'+j"
+          xl="2"
+          lg="2"
+          md="3"
+          sm="4"
+          cols="6"
+        >
+          <router-link :to="{ name: 'watch', params: { v: video.id.videoId }}">
+            <v-card class="video-card" height="150">
+              <v-img :src="video.snippet.thumbnails.high.url" width="100%" height="100%"></v-img>
+              <div class="video-overlay">
+                <p>{{video.snippet.title}}</p>
+              </div>
+            </v-card>
+          </router-link>
+        </v-col>
+      </v-row>
+      <div v-else class="no-results-container">
+        <p>Your search for "{{$route.params.q}}" did not have any matches.</p>
+        <p>Suggestions:</p>
+        <ul>
+          <li v-for="suggestion in suggestions" :key="suggestion">{{suggestion}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -37,6 +42,7 @@ import { of } from "rxjs";
 import {
   debounceTime,
   distinctUntilChanged,
+  // finalize,
   map,
   switchMap
 } from "rxjs/operators";
@@ -44,9 +50,14 @@ import {
 export default {
   data() {
     return {
+      loaded: true,
       videos: [],
       youtubeGet: new YouTubeGet(),
-      suggestions:['Try different keywords','Looking for a video or channel?','Try a genre, like comedy, romance, sports, or drama']
+      suggestions: [
+        "Try different keywords",
+        "Looking for a video or channel?",
+        "Try a genre, like comedy, romance, sports, or drama"
+      ]
     };
   },
   watch: {
@@ -59,11 +70,19 @@ export default {
           debounceTime(1000),
           distinctUntilChanged()
         )
-        .subscribe();
+        .subscribe({
+          complete: () => {
+            this.loading = false;
+          }
+        });
     }
   },
   mounted() {
-    this.getSearchResults(this.$route.params.q).subscribe();
+    this.getSearchResults(this.$route.params.q).subscribe({
+      complete: () => {
+        this.loading = false;
+      }
+    });
   },
   methods: {
     queryChange() {
@@ -71,12 +90,16 @@ export default {
     },
     getSearchResults(query) {
       const searchEP = process.env.VUE_APP_SEARCH_ENDPOINT;
+      this.loading = true;
       return this.youtubeGet
         .getVideos(searchEP, `part=snippet&type=video&q=${query}&maxResults=24`)
         .pipe(
           map(res => {
             this.videos = res.items;
           })
+          // finalize(() => {
+          //   this.loading = false;
+          // })
         );
     }
   }
